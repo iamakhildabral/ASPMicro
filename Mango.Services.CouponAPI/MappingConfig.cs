e9beacce29626ec56a486ec99881ec9ffc3a821d
iamakhildabral <akhildabral02@gmail.com>
2023-08-28 23:35:54 +0530

    add dto and automapper

---
+++ Mango.Services.CouponAPI/MappingConfig.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Mango.Services.CouponAPI.Models;
+using Mango.Services.CouponAPI.Models.DTO;
+
+namespace Mango.Services.CouponAPI;
+/*
+ * We are creating this class because we need frequent conversion of the DTO class to model/entity
+ * class and for that we need automapper help.
+ */
+public class MappingConfig
+{
+    public static MapperConfiguration RegisterMaps() 
+    {
+        var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<CouponDto, Coupon>();
+                config.CreateMap<Coupon, CouponDto>();
+            });
+        return mappingConfig;
+    }
+}
+/*
+ * Now register this service in the program.cs
+ */
---
+++ Mango.Services.CouponAPI/Models/DTO/ResponseDTO.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * Setting this class to make a common response for all
+ * The called APIs.
+ * Since Resulting Objecgt, Message and SuccessCode need to be passed
+ * we have created this class with those parameters
+ */
+
+namespace Mango.Services.CouponAPI.Models.DTO
+{
+    public class ResponseDTO
+    {
+        public object? Result { get; set; }
+        public bool Success { get; set; } = true;
+        public String Message { get; set; } = String.Empty;
+
+    }
+}
